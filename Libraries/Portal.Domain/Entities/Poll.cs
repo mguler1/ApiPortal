5ca13603ad2715703b0d8591e2f3655e3cf108d2
= <=>
2021-03-01 16:22:20 +0300

    poll models eklendi

---
+++ Libraries/Portal.Domain/Entities/Poll.cs
@@ -0,0 +1,16 @@
+﻿using Portal.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.Entities
+{
+    public class Poll:BaseEntity
+    {
+        public string  Name { get; set; }
+        public bool  ShowOnHomePage { get; set; }
+        public bool  AllowGuetsToVote { get; set; }
+        public DateTime  StartDate { get; set; }
+        public DateTime?  EndDate { get; set; }
+    }
+}
---
+++ Libraries/Portal.Domain/Entities/PollAnswer.cs
@@ -0,0 +1,15 @@
+﻿using Portal.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.Entities
+{
+  public  class PollAnswer:BaseEntity
+    {
+        public int PollId { get; set; }
+        public virtual Poll Poll { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
