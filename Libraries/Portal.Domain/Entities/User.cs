2d40b797e7a7504ea17098b921ef9f8b4037d2ed
= <=>
2021-03-01 16:04:02 +0300

    Course Entity eklendi

--- Libraries/Portal.Domain/Entities/User.cs
+++ Libraries/Portal.Domain/Entities/User.cs
@@ -13,6 +13,7 @@ namespace Portal.Domain.Entities
        public virtual ICollection<Article> Articles { get; set; }//kullanıcının birden fazla makalesi olabilir
        public virtual ICollection<Comment> Comments { get; set; }//kullanıcının birden fazla yorumu olabilir
        public virtual ICollection<Contact> Contacts { get; set; }//
+       public virtual ICollection<CourseLesson> CourseLessons { get; set; }//
 
     }
 }
