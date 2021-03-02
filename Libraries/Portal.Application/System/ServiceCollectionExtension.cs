3b051798fe726d4e2b1c8da5fbd1d8621223fac8
= <=>
2021-03-02 14:13:42 +0300

    Application ve Api projesi eklendi

---
+++ Libraries/Portal.Application/System/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Portal.Infastructure.Persitence;
+using Portal.Infastructure.Persitence.EntityFreamwork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Application.System
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection InjectApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>),typeof(EfRepository<>));
+            return services;
+        }
+        public static IServiceCollection AddCustomizedDataStore(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<PortalDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),b=>b.MigrationsAssembly("Portal.Infastructure")));
+            services.AddScoped<DbContext>(provider => provider.GetService<PortalDbContext>());
+            return services;
+        }
+    }
+}
