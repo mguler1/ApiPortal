dff50062444afe2cc13abac8198f47415b5ba39e
= <=>
2021-03-02 12:06:03 +0300

    IRepository oluşturuldu

---
+++ Libraries/Portal.Infastructure/Persitence/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using Portal.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Portal.Infastructure.Persitence
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        T GEtById(int id);
+        IQueryable<T>GetAll { get;}
+        IQueryable<T> GetAllNoTracking { get; }
+        T Get(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes);
+        IQueryable<T> GetMany(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes);
+        T InsertWithoutCommit(T entity);
+
+        void InsertBulk(IEnumerable<T> entities);
+
+        void Update(T entity);
+        void UpdateWithoutCommit(T entity);
+
+        int Delete(T entity);
+        int Remove(T entity);
+
+        int Commit();
+        void DeleteBulk(IEnumerable<T> entities);
+        bool Any(Expression<Func<T, bool>> expression);
+    }
+}
